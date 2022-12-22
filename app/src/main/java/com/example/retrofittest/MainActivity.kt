3ba0043d61b229280132b4b2a492ad5c0b283ef0
jakeandrey.suerte <jakeandrey.suerte@awsys-i.com>
2022-12-22 15:37:07 +0800

    init

---
+++ app/src/main/java/com/example/retrofittest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.retrofittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+     override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+     }
+}
