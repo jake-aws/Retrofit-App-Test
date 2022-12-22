c0d44b0f3e8c4238ed0b39331b56256081431853
jakeandrey.suerte <jakeandrey.suerte@awsys-i.com>
2022-12-22 16:43:36 +0800

    Retrofit Call

--- app/src/main/java/com/example/retrofittest/MainActivity.kt
+++ app/src/main/java/com/example/retrofittest/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.retrofittest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.example.retrofittest.databinding.ActivityMainBinding
+import com.example.retrofittest.dummyJSON.Product
 import com.example.retrofittest.dummyJSON.ProductService
 import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
@@ -11,21 +14,36 @@ import retrofit2.converter.moshi.MoshiConverterFactory
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var productService: ProductService
+    lateinit var retrofit: Retrofit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val retrofit = Retrofit.Builder().baseUrl("https://dummyjson.com/")
+        retrofit = Retrofit.Builder().baseUrl("https://dummyjson.com/")
             .addConverterFactory(MoshiConverterFactory.create()).build()
-        val productsCompatActivity: ProductService = retrofit.create(ProductService::class.java)
+        productService = retrofit.create(ProductService::class.java)
 
-        productsCompatActivity.getProductByID().enqueue(object : Callback<Any?> {
-            override fun onResponse(call: Call<Any?>, response: Response<Any?>) {
+        getItem(23)
+    }
+
+    private fun getItem(id: Int) {
+        productService.getProductByID(id).enqueue(object : Callback<Product> {
+            override fun onResponse(call: Call<Product>, response: Response<Product>) {
                 Log.i("MainActivity", response.toString())
 
+                if (!response.isSuccessful) {
+                    Toast.makeText(this@MainActivity, "Request failed", Toast.LENGTH_SHORT).show()
+                    return
+                }
+                val body = response.body()!!
+                val brand = body.brand
+                binding.tvTextBrand.text = brand
             }
 
-            override fun onFailure(call: Call<Any?>, t: Throwable) {
+            override fun onFailure(call: Call<Product>, t: Throwable) {
                 Log.i("MainActivity", t.message ?: "Null Message")
             }
         })
--- app/src/main/java/com/example/retrofittest/dummyJSON/ProductService.kt
+++ app/src/main/java/com/example/retrofittest/dummyJSON/ProductService.kt
@@ -2,9 +2,10 @@ package com.example.retrofittest.dummyJSON
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 
 interface ProductService {
-    @GET("products/1")
-    fun getProductByID():Call<Any>
+    @GET("products/{product_ID}")
+    fun getProductByID(@Path("product_ID") productID:Int):Call<Product>
 }
